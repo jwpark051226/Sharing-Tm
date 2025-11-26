b50d023e0cb25a1e8f8d90052e5ea7d04aeaf2d2
jwpark051226 <jwpark051226@tukorea.ac.kr>
2025-11-26 15:56:22 +0900

    Update Program.cs

--- C#-Original/Program.cs
+++ C#-Original/Program.cs
@@ -1,4 +1,4 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using SharingTm.Model;
 using System;
 using System.Collections.Generic;
@@ -13,6 +13,7 @@ namespace SharingTm
     {
         static void Main(string[] args)
         {
+            // 프로그램 시작 시 DB 연결 상태 확인
             if (DatabaseManager.TestConnection())
             {
                 bool mainQuit = false;
@@ -46,6 +47,7 @@ namespace SharingTm
             }
         }
 
+        // 미구현
         private static void UserMenu()
         {
             bool userQuit = false;
@@ -54,9 +56,11 @@ namespace SharingTm
                 Console.WriteLine("1. 로그인");
                 Console.WriteLine("2. 회원가입");
                 Console.WriteLine("0. 메뉴 선택으로 돌아가기\n");
-            }           
+                userQuit = true; 
+            }            
         }
         
+        // 관리자 전용 메뉴 진입
         private static void AdminMenu()
         {
             bool adminQuit = false;
@@ -89,7 +93,6 @@ namespace SharingTm
             }
         }
 
-        // TODO 사용자 추가기능 생각해보기
         private static void AdminUserManage()
         {
             bool userMenuQuit = false;
@@ -158,6 +161,7 @@ namespace SharingTm
 
         private static void AdminViewUser()
         {
+            // DB에서 전체 유저 목록 가져오기
             List<User> userListForView = DatabaseManager.GetUsers();
             if (userListForView.Count > 0)
             {
@@ -174,6 +178,8 @@ namespace SharingTm
             if (newUserName.Length <= 45)
             {
                 List<User> userList = DatabaseManager.GetUsers();
+                
+                // 중복된 이름이 있는지 확인하여 처리하는 로직
                 if (userList.Any(
                     user => user.username.Equals(newUserName)))
                 {
@@ -181,6 +187,7 @@ namespace SharingTm
                     bool flag = false;
                     while (!flag)
                     {
+                        // 중복 시 이름 뒤에 (n)을 붙여 유니크한 이름 생성
                         if (userList.Any(
                              user => user.username.Equals($"{newUserName}({i})")))
                             i++;
@@ -191,6 +198,8 @@ namespace SharingTm
                         }
                     }
                 }
+                
+                // 최종 결정된 이름으로 DB에 추가
                 if (DatabaseManager.AddNewUser(newUserName))
                     Console.WriteLine($"{newUserName} 사용자가 추가되었습니다.");
                 else Console.WriteLine("사용자 추가에 실패했습니다.");
@@ -229,6 +238,7 @@ namespace SharingTm
             }
         }
 
+        // 목록을 보고 ID를 직접 입력하여 삭제
         private static void DelUserWithSelect()
         {
             List<User> userList = DatabaseManager.GetUsers();
@@ -259,6 +269,7 @@ namespace SharingTm
                 Console.WriteLine("등록된 사용자가 없습니다.");
         }
 
+        // 이름으로 검색하여 삭제 (동명이인 처리 포함)
         private static void DelUserWithName()
         {
             Console.Write("검색할 사용자의 이름을 입력해주세요 : ");
@@ -270,6 +281,7 @@ namespace SharingTm
             {
                 PrintUserList(userList);
 
+                // 검색 결과가 1명일 경우 바로 삭제 의사 확인
                 if (userList.Count == 1)
                 {
                     bool hasConfirmed = false;
@@ -285,7 +297,7 @@ namespace SharingTm
                             else
                                 Console.WriteLine("사용자를 제거할 수 없었습니다.");
                         }
-                        else if (confirmDel.ToLower().Equals("y"))
+                        else if (confirmDel.ToLower().Equals("y")) // (참고: 로직상 n에 대한 처리가 누락되어 있거나 중복 조건으로 보임)
                         {
                             hasConfirmed = true;
                         }
@@ -295,6 +307,7 @@ namespace SharingTm
                         }
                     }
                 }
+                // 검색 결과가 여러 명일 경우 ID로 특정하여 삭제
                 else
                 {
                     bool selectedUserIdExists_1 = false;
@@ -319,10 +332,11 @@ namespace SharingTm
             }
         }
 
+        // 유저 리스트 출력 포맷팅
         private static void PrintUserList(List<User> userList)
         {
             Console.WriteLine("-----------------------------------------------------");
-            Console.WriteLine(" ID    이름         잔액         이용권 유무");
+            Console.WriteLine(" ID    이름          잔액          이용권 유무");
             Console.WriteLine("-----------------------------------------------------");
             foreach (User user in userList)
             {
@@ -414,6 +428,7 @@ namespace SharingTm
                     int delStationID = int.Parse(Console.ReadLine());
                     if (delStationID == 0)
                         selectedStationIDExists = true;
+                    // 입력한 ID가 리스트에 존재하는지 검증
                     else if (stationList.Any(station => station.stationID == delStationID))
                     {
                         selectedStationIDExists = true;
@@ -440,6 +455,8 @@ namespace SharingTm
             else
             {
                 PrintStationList(stationList);
+                
+                // 검색된 대여소가 1개일 때 처리
                 if (stationList.Count == 1)
                 {
                     bool hasConfirmed = false;
@@ -465,6 +482,7 @@ namespace SharingTm
                         }
                     }
                 }
+                // 검색된 대여소가 여러 개일 때 ID로 선택
                 else
                 {
                     bool selectedStationIDExists = false;
@@ -477,7 +495,7 @@ namespace SharingTm
                         else if (stationList.Any(station => station.stationID == delStationID))
                         {
                             selectedStationIDExists = true;
-                            if (DatabaseManager.DeleteUser(delStationID))
+                            if (DatabaseManager.DeleteStation(delStationID))
                                 Console.WriteLine("대여소를 성공적으로 제거했습니다.");
                             else
                                 Console.WriteLine("대여소를 제거할 수 없었습니다.");
@@ -537,7 +555,7 @@ namespace SharingTm
                         else if (stationList.Any(station => station.stationID == delStationID))
                         {
                             selectedStationIDExists = true;
-                            if (DatabaseManager.DeleteUser(delStationID))
+                            if (DatabaseManager.DeleteStation(delStationID))
                                 Console.WriteLine("대여소를 성공적으로 제거했습니다.");
                             else
                                 Console.WriteLine("대여소를 제거할 수 없었습니다.");
@@ -549,6 +567,7 @@ namespace SharingTm
             }
         }
 
+        // 대여소 리스트 출력 포맷팅
         private static void PrintStationList(List<Station> StationList)
         {
             Console.WriteLine("\n------------------------------------------------------------------------------------------");
